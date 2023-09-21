db240e9c79f43baaa1c93b032859e180a9c0e3c1
MainDepth <amin.awal@hotmail.co.uk>
2023-09-21 01:26:09 +0100

    finished the to do list application

---
+++ to_Do_list_application/To_do_list.py
@@ -0,0 +1,35 @@
+import To_do_list_class 
+
+
+def main():
+    my_list = To_do_list_class.ToDoList()
+
+    while True:     
+        print("================================")
+        print("1: view tasks ")
+        print("2: add task ")
+        print("3: mark task as completed ")
+        print("================================")
+
+        a =input("Select an option ")
+
+
+
+        if a == "1":
+           my_list.view_task()
+
+        elif a == "2": 
+            name = input(str("name of task: "))
+            desctription = input(str("task description: "))
+            duration = input(str("duration of task: "))
+            my_list.add_task(name,desctription, duration)
+
+        elif a == "3":
+            TaskID= int(input("enter the task number to mark as completed: "))
+            my_list.mark_task_as_completed(TaskID)
+
+        else:
+            print("option not available")
+
+if __name__ == "__main__":
+    main()
--- to_Do_list_application/To_do_list_class.py
+++ to_Do_list_application/To_do_list_class.py
@@ -13,11 +13,6 @@ class Task:
         return f"Task name: {self.taskname}, task description: {self.task_description},task duration: {self.task_duration}, task completed: {taskCompleted}"
     
 
-
-
-
-
-
 class ToDoList:
     
     def __init__(self):
@@ -43,37 +38,3 @@ class ToDoList:
         else:
             print("invalid task id")
 
- 
-
-def main():
-    my_list = ToDoList()
-
-    while True:     
-        print("================================")
-        print("1: view tasks ")
-        print("2: add task ")
-        print("3: mark task as completed ")
-        print("================================")
-
-        a =input("Select an option ")
-
-
-
-        if a == "1":
-           my_list.view_task()
-
-        elif a == "2": 
-            name = input(str("name of task: "))
-            desctription = input(str("task description: "))
-            duration = input(str("duration of task: "))
-            my_list.add_task(name,desctription, duration)
-
-        elif a == "3":
-            TaskID= int(input("enter the task number to mark as completed"))
-            my_list.mark_task_as_completed(TaskID)
-
-        else:
-            print("option not available")
-
-if __name__ == "__main__":
-    main()
